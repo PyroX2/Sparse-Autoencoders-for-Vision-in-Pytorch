5ecf87600b2f8b07daf6c6ad4982a91739838910
Jakub Wilk <kubawilk63@gmail.com>
2026-06-26 14:31:21 +0200

    Update show_from_dataset function to show images at specified indicies

--- utils.py
+++ utils.py
@@ -9,20 +9,24 @@ def show_from_dataset(dataset, idx: int | typing.List = 0):
         plt.title(f"Label: {dataset[idx][1].item()}")
         plt.axis("off")
     else:
+        current_idx = 0
         if len(idx) >= 5:
             fig, axs = plt.subplots(len(idx) // 5, 5)
             for i, subaxs in enumerate(axs):
                 for j, ax in enumerate(subaxs):
-                    img_idx = i * 5 + j
+                    img_idx = idx[current_idx]
                     ax.imshow(dataset[img_idx][0].permute(1, 2, 0).numpy())
                     ax.set_title(f"Label: {dataset[img_idx][1].item()}")
                     ax.axis("off")
+                    current_idx += 1
         else:
             fig, axs = plt.subplots(1, len(idx))
             for i, ax in enumerate(axs):
-                ax.imshow(dataset[i][0].permute(1, 2, 0).numpy())
-                ax.set_title(f"Label: {dataset[i][1].item()}")
+                img_idx = idx[current_idx]
+                ax.imshow(dataset[img_idx][0].permute(1, 2, 0).numpy())
+                ax.set_title(f"Label: {dataset[img_idx][1].item()}")
                 ax.axis("off")
+                current_idx += 1
     plt.show()
 
 
