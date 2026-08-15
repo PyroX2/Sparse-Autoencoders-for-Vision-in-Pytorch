b6cbff1c39adb608c1ee4707db082c37300f8565
Jakub Wilk <kubawilk63@gmail.com>
2026-08-15 12:14:06 +0200

    Add get_device function

--- src/train/classifier.py
+++ src/train/classifier.py
@@ -1,19 +1,25 @@
-import torch
 import logging
+import torch
+import sklearn
 from src.configs import configs
 from src.data import data
+from src.models import ClsModel
+
+
+def get_device():
+    if torch.cuda.is_available():
+        return "cuda"
+    elif torch.backends.mps.is_available():
+        return "mps"
+    else:
+        return "cpu"
 
 
 def main():
-    config = configs.ClsTrainConfig.from_yaml('config/train_cls_config.yaml')
-    logger = logging.Logger('Training logger', config.general.logging_level)
+    config = configs.ClsTrainConfig.from_yaml("config/train_cls_config.yaml")
+    logger = logging.Logger("Training logger", config.general.logging_level)
 
-    if torch.cuda.is_available():
-        device = "cuda"
-    elif torch.backends.mps.is_available():
-        device = "mps"
-    else:
-        device = "cpu"
+    device = get_device()
 
     train_dataset = data.get_dataset(config.dataset.name, split="train")
     test_dataset = data.get_dataset(config.dataset.name, split="test")
@@ -23,6 +29,14 @@ def main():
                 Training dataset:\n{train_dataset}\n
                 Test dataset:\n{test_dataset}""")
 
+    # Split train dataset into train and val while maintaining labels ratio
+    labels = torch.tensor([y for x, y in train_dataset])
+    train_split, val_split = sklearn.model_selection.train_test_split(
+        train_dataset,
+        test_size=config.hyperparams.val_size,
+        stratify=labels,
+        random_state=config.general.seed,
+    )
 
 
 if __name__ == "__main__":
